ebc57f879095b11f24b8c4ce7ac06d8580a80704
arielbk <buchwald.kearney@gmail.com>
2019-01-02 19:37:13 +0100

    Setup redux, action types, reducers and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,81 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import logo from './logo.svg';
 import MoviesList from './MoviesList';
 import MovieDetail from './MovieDetail';
 import SearchButton from './SearchButton';
 
+// Check for token
+if (localStorage.jwtToken) {
+	// Set auth token to header auth
+	setAuthToken(localStorage.jwtToken);
+	// Decode token and get user info and exp
+	const decoded = jwt_decode(localStorage.jwtToken);
+
+	// Check for expired token
+	const currentTime = Date.now() / 1000;
+	if (decoded.exp < currentTime) {
+		// Log user out
+		store.dispatch(logoutUser());
+		// Redirect to login
+		window.location.href = '/login';
+	} else {
+		// Set user and isAuthenticated
+		store.dispatch(setCurrentUser(decoded));
+	}
+}
+
 const App = () => (
-  <Router>
-    <StyledApp>
-      <GlobalStyle />
-      <Header>
-        {/* force the movieslist component to refetch data... */}
-        <Link to="/" onClick={() => setTimeout(window.location.reload(true))}>
-          <Logo src={logo} alt="logo" />
-        </Link>
-        <SearchButton />
-      </Header>
-      <Switch>
-        <Route exact path="/" component={MoviesList} />
-        <Route exact path="/p=:page" component={MoviesList} />
-        <Route exact path="/search=:query" component={MoviesList} />
-        <Route path="/search=:query/p=:page" component={MoviesList} />
-        <Route path="/:id" component={MovieDetail} />
-      </Switch>
-    </StyledApp>
-  </Router>
+	<Provider store={store}>
+		<Router>
+			<StyledApp>
+				<GlobalStyle />
+				<Header>
+					{/* force the movieslist component to refetch data... */}
+					<Link to="/" onClick={() => setTimeout(window.location.reload(true))}>
+						<Logo src={logo} alt="logo" />
+					</Link>
+					<SearchButton />
+				</Header>
+				<Switch>
+					<Route exact path="/" component={MoviesList} />
+					<Route exact path="/p=:page" component={MoviesList} />
+					<Route exact path="/search=:query" component={MoviesList} />
+					<Route path="/search=:query/p=:page" component={MoviesList} />
+					<Route path="/:id" component={MovieDetail} />
+				</Switch>
+			</StyledApp>
+		</Router>
+	</Provider>
 );
 
 export default App;
 
 const Header = styled.div`
-  background-color: #111;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: #111;
+	background-color: #111;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: #111;
 `;
 
 const StyledApp = styled.div`
-  text-align: center;
+	text-align: center;
 `;
 
 const Logo = styled.img`
-  margin: 2rem;
+	margin: 2rem;
 `;
 
 const GlobalStyle = createGlobalStyle`
