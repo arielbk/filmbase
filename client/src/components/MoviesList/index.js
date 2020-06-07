f860a6b70a315fdc4d5b0ba5a9baf4ca5af74c24
arielbk <buchwald.kearney@gmail.com>
2020-06-07 14:14:42 +0200

    Basic react query working

--- client/src/components/MoviesList/index.js
+++ client/src/components/MoviesList/index.js
@@ -1,9 +1,14 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ReactStars from 'react-stars';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { updateFilmList, setListPage, setSearchQuery, setSortBy } from '../../actions/listActions';
 import { Redirect } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import Axios from 'axios';
+import { updateFilmList, setListPage, setSearchQuery, setSortBy } from '../../actions/listActions';
+
+import useIntersectionObserver from '../../hooks/useIntersectionObserver';
 
 import { MovieGrid, GenreTab, GenreList } from './MoviesList.styled';
 
@@ -14,154 +19,125 @@ import LoadMore from '../LoadMore';
 import Movie from '../Movie';
 import genres from '../../assets/genres';
 
-class MoviesList extends Component {
-	state = {
-		searchQuery: '',
+const MoviesList = ({ match, ...props }) => {
+	const [searchQuery, setSearchQuery] = useState('');
+	const [sortBy, setSortBy] = useState('popular.desc');
+	const [page, setPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(10);
+	let voteCount = 100;
+	// Require at least 500 votes for the 'top rated' category
+	if (sortBy === 'vote_average.desc') voteCount = 500;
+
+	const fetchFilms = async () => {
+		const apiURL = searchQuery
+			? `https://api.themoviedb.org/3/search/movie?api_key=5f65a05aa95f0f49a243118f362a4d69&language=en-US&query=${searchQuery}&page=${page}&include_adult=false`
+			: `https://api.themoviedb.org/3/discover/movie?api_key=5f65a05aa95f0f49a243118f362a4d69&language=en-US&sort_by=${sortBy}&include_adult=false&include_video=false&page=${page}&vote_count.gte=${voteCount}`;
+		const { data } = await axios.get(apiURL);
+		return data.results;
 	};
 
-	static propTypes = {
-		match: PropTypes.shape({
-			params: PropTypes.shape({
-				page: PropTypes.string,
-				searchQuery: PropTypes.string,
-			}),
-		}),
-		list: PropTypes.shape({
-			films: PropTypes.array.isRequired,
-			loading: PropTypes.bool.isRequired,
-			page: PropTypes.number,
-			searchQuery: PropTypes.string,
-			sortBy: PropTypes.string,
-		}).isRequired,
-		showHearted: PropTypes.bool,
-	};
+	const { status, data, error, isFetching } = useQuery(
+		['films', { searchQuery, sortBy }],
+		fetchFilms
+	);
+	console.log({ status, data, error, isFetching });
+	const films = data;
 
-	static defaultProps = {
-		match: {
-			params: {
-				page: '1',
-				searchQuery: null,
-			},
-		},
-		list: {
-			page: 1,
-			searchQuery: null,
-			sortBy: 'popular.desc',
-		},
-		hearted: PropTypes.object.isRequired,
-		showHearted: false,
-	};
+	// useEffect(() => {
+	// 	newList();
+	// }, []);
 
-	async componentDidMount() {
-		this.newList();
+	// const newList = () => {
+	// 	const { query } = this.props.match.params;
+	// 	const { sortBy } = this.props.list;
+	// 	setListPage(1);
+	// 	this.props.setSearchQuery(query);
+	// 	this.props.updateFilmList(1, query, sortBy);
+	// 	this.setState({ searchQuery: query });
+	// };
+
+	const { showHearted } = props;
+	const { user, isAuthenticated } = props.auth;
+	// let films;
+	// if (showHearted) {
+	// 	const { hearted } = this.props.hearted;
+	// 	films = hearted;
+	// } else {
+	// 	films = props.list.films;
+	// }
+	let topComponent;
+	if (!showHearted) {
+		!searchQuery
+			? (topComponent = <SortOptions sortBy={sortBy} setSortBy={setSortBy} />)
+			: (topComponent = (
+					<>
+						<h3>Search term:</h3>
+						{searchQuery}
+					</>
+			  ));
+	} else {
+		topComponent = <h1>Hearted Films for {user.name}</h1>;
 	}
+	return (
+		<>
+			{showHearted && !isAuthenticated && <Redirect to="/login" />}
 
-	static getDerivedStateFromProps(nextProps, prevState) {
-		if (nextProps.match.params.query !== prevState.searchQuery)
-			return { searchQuery: nextProps.match.params.query };
-		return null;
-	}
+			{topComponent}
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevProps.match.params.query !== this.props.match.params.query) {
-			this.newList();
-		}
-	}
+			{isFetching ? (
+				<Loading />
+			) : (
+				<MovieGrid data-testid="movie-results">
+					{films.map(movie => (
+						<Movie key={movie.id} movie={movie}>
+							<h3 data-testid="movieposter-title">{movie.title}</h3>
+							<h5 data-testid="movieposter-year">
+								{movie.release_date.split('-')[0]}
+							</h5>
+							<ReactStars
+								count={5}
+								value={movie.vote_average / 2}
+								size={24}
+								color2={$brandGreen}
+								edit={false}
+							/>
+							<GenreList>
+								{movie.genre_ids &&
+									movie.genre_ids.length &&
+									movie.genre_ids.map(genreID => (
+										<span key={genreID}>
+											{genres.map(
+												genre =>
+													genreID === genre.id && (
+														<GenreTab
+															data-testid="movieposter-genre"
+															key={genre.name}
+														>
+															{genre.name}
+														</GenreTab>
+													)
+											)}
+										</span>
+									))}
+							</GenreList>
+						</Movie>
+					))}
+				</MovieGrid>
+			)}
 
-	newList = () => {
-		const { query } = this.props.match.params;
-		const { sortBy } = this.props.list;
-		this.props.setListPage(1);
-		this.props.setSearchQuery(query);
-		this.props.updateFilmList(1, query, sortBy);
-		this.setState({ searchQuery: query });
-	};
+			{!isFetching && !films.length && (
+				<h2
+					data-testid="movie-results"
+					style={{ position: 'relative', top: '-4rem', fontWeight: 200 }}
+				>
+					No films found!
+				</h2>
+			)}
 
-	render() {
-		const { showHearted } = this.props;
-		const { user, isAuthenticated } = this.props.auth;
-		const { searchQuery, loading, sortBy, page, totalPages } = this.props.list;
-		let films;
-		if (showHearted) {
-			const { hearted } = this.props.hearted;
-			films = hearted;
-		} else {
-			films = this.props.list.films;
-		}
-		let topComponent;
-		if (!showHearted) {
-			!searchQuery
-				? (topComponent = <SortOptions sortBy={sortBy} />)
-				: (topComponent = (
-						<Fragment>
-							<h3>Search term:</h3>
-							{searchQuery}
-						</Fragment>
-				  ));
-		} else {
-			topComponent = <h1>Hearted Films for {user.name}</h1>;
-		}
-		return (
-			<Fragment>
-				{showHearted && !isAuthenticated && <Redirect to="/login" />}
-
-				{topComponent}
-
-				{loading ? (
-					<Loading />
-				) : (
-					<MovieGrid data-testid="movie-results">
-						{films.map(movie => (
-							<Movie key={movie.id} movie={movie}>
-								<h3 data-testid="movieposter-title">{movie.title}</h3>
-								<h5 data-testid="movieposter-year">
-									{movie.release_date.split('-')[0]}
-								</h5>
-								<ReactStars
-									count={5}
-									value={movie.vote_average / 2}
-									size={24}
-									color2={$brandGreen}
-									edit={false}
-								/>
-								<GenreList>
-									{movie.genre_ids &&
-										movie.genre_ids.length &&
-										movie.genre_ids.map(genreID => (
-											<span key={genreID}>
-												{genres.map(
-													genre =>
-														genreID === genre.id && (
-															<GenreTab
-																data-testid="movieposter-genre"
-																key={genre.name}
-															>
-																{genre.name}
-															</GenreTab>
-														)
-												)}
-											</span>
-										))}
-								</GenreList>
-							</Movie>
-						))}
-					</MovieGrid>
-				)}
-
-				{!loading && !films.length && (
-					<h2
-						data-testid="movie-results"
-						style={{ position: 'relative', top: '-4rem', fontWeight: 200 }}
-					>
-						No films found!
-					</h2>
-				)}
-
-				{!showHearted && !loading && page < totalPages && <LoadMore />}
-			</Fragment>
-		);
-	}
-}
+			{!showHearted && !isFetching && page < totalPages && <LoadMore />}
+		</>
+	);
+};
 
 const mapStateToProps = state => ({
 	list: state.list,
@@ -169,7 +145,6 @@ const mapStateToProps = state => ({
 	hearted: state.hearted,
 });
 
-export default connect(
-	mapStateToProps,
-	{ updateFilmList, setListPage, setSearchQuery, setSortBy }
-)(MoviesList);
+export default connect(mapStateToProps, { updateFilmList, setListPage, setSearchQuery, setSortBy })(
+	MoviesList
+);
--- client/src/components/SortOptions/index.js
+++ client/src/components/SortOptions/index.js
@@ -1,53 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyledSortOptions, SortButton } from './SortOptions.styled';
-import { connect } from 'react-redux';
 import { setSortBy, updateFilmList, setListPage } from '../../actions/listActions';
 
-class SortOptions extends Component {
-	handleChangeSort = async newSorting => {
-		await this.props.setSortBy(newSorting);
-		await this.props.setListPage(1);
-		this.props.updateFilmList(
-			this.props.list.page,
-			this.props.list.searchQuery,
-			this.props.list.sortBy
-		);
-	};
+const SortOptions = ({ sortBy, setSortBy }) => {
+	return (
+		<StyledSortOptions>
+			<h4>Sort by:</h4>
+			<SortButton
+				active={sortBy === 'popularity.desc'}
+				onClick={() => setSortBy('popularity.desc')}
+			>
+				most popular
+			</SortButton>
+			<SortButton
+				active={sortBy === 'release_date.desc'}
+				onClick={() => setSortBy('release_date.desc')}
+			>
+				newest
+			</SortButton>
+			<SortButton
+				active={sortBy === 'vote_average.desc'}
+				onClick={() => setSortBy('vote_average.desc')}
+			>
+				top rated
+			</SortButton>
+		</StyledSortOptions>
+	);
+};
 
-	render() {
-		const { list } = this.props;
-		const { sortBy } = list;
-		return (
-			<StyledSortOptions>
-				<h4>Sort by:</h4>
-				<SortButton
-					active={sortBy === 'popularity.desc'}
-					onClick={() => this.handleChangeSort('popularity.desc')}
-				>
-					most popular
-				</SortButton>
-				<SortButton
-					active={sortBy === 'release_date.desc'}
-					onClick={() => this.handleChangeSort('release_date.desc')}
-				>
-					newest
-				</SortButton>
-				<SortButton
-					active={sortBy === 'vote_average.desc'}
-					onClick={() => this.handleChangeSort('vote_average.desc')}
-				>
-					best rated
-				</SortButton>
-			</StyledSortOptions>
-		);
-	}
-}
-
-const mapStateToProps = state => ({
-	list: state.list,
-});
-
-export default connect(
-	mapStateToProps,
-	{ setSortBy, updateFilmList, setListPage }
-)(SortOptions);
+export default SortOptions;
